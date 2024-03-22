ccd6c9e8f57f51660c065e1f8c6e562dd25ae69a
GhazalKa <37194793+GhazalKa@users.noreply.github.com>
2024-03-22 09:55:05 +0330

    the temperature unit next to icon is not updating!

--- src/index.js
+++ src/index.js
@@ -1,12 +1,31 @@
 // alert("hi");
+//declaring seprated functions is good seprate concern
+
+function refreshWeather(response) {
+  console.log(response.data.temperature.current);
+  let temperatureElement = document.querySelector("#temperature");
+  let temperature = response.data.temperature.current;
+  let cityElement = document.querySelector("#city");
+  // console.log("cityElement.value");
+  cityElement.innerHTML = response.data.city;
+
+  temperature.innerHTML = Math.round(temperature);
+}
+function searchcity(city) {
+  //make api call and update the interface
+  let apiKey = "5362bd34cf050a03d30tbfffa6oc1faa";
+  let apiUrl = `https://api.shecodes.io/weather/v1/current?query=${city}&key=${apiKey}&unit=metric`;
+  console.log(apiUrl);
+  axios.get(apiUrl).then(refreshWeather);
+}
 function handleSearchSubmit(event) {
   //   console.log(searchFormElement);
   event.preventDefault();
   let searchInput = document.querySelector("#search-form-input");
   //   console.log(searchInput.value);
-  let cityElement = document.querySelector("#city");
-  // console.log("cityElement.value");
-  cityElement.innerHTML = searchInput.value;
+
+  //the value of search input is send to above function by the following code
+  searchcity(searchInput.value);
 }
 
 let searchFormElement = document.querySelector("#search-form");
